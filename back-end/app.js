c40557e7f608d65de18320aabaf5b0f31681504a
Mathieu <mlecherb@MacBook-Air-de-Mathieu.local>
2024-04-22 17:41:47 +0200

    askRoom event

--- back-end/app.js
+++ back-end/app.js
@@ -41,6 +41,9 @@ io.on('connection', (socket) => {
         }
         user.push({socket, client: data.client});
         socket.emit('UsernameOK', {username: data.client}); //send back the allowed name so frontend knows what name to display
+    })
+
+    socket.on('askRoom', () => {
         sendToAllUser(user, 'roomUpdate', rooms);
     })
 
